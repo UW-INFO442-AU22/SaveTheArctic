900f32aec45f3efbf00cc836e8ccf5783f7e61bd
Catherine Miao <md0615@uw.edu>
2022-11-15 15:12:31 -0800

    edit navbar

--- save-arctic/src/components/layout/NavBar.js
+++ save-arctic/src/components/layout/NavBar.js
@@ -1,20 +1,23 @@
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Navbar, NavItem, NavbarBrand} from 'react-bootstrap';
 
 
 function NavBar() {
     return (
     <Navbar bg="primary" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Save The Arctic</Navbar.Brand>
+        <NavbarBrand href="/" className="navbar-brand">Save The Arctic</NavbarBrand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Education</Nav.Link>
-            <Nav.Link href="#link">My Points</Nav.Link>
+            <NavItem>
+              <Link to="/education" className="nav-link">Education</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/point" className="nav-link">My Points</Link>
+            </NavItem>
           </Nav>
         </Navbar.Collapse>
       </Container>
