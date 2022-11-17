9f60260ecfa478350a358f5d733544c2f18bf7af
张巧依 <zhangqiaoyi@Whitney-Zhang.local>
2022-11-16 16:18:18 -0800

    format education page

---
+++ save-arctic/src/data/animals.js
@@ -0,0 +1,63 @@
+{
+    "animal" [
+        {
+            "id":"1",
+            "name":"Arctic Fox",
+            "Facts":"The Arctic Fox is one of the most endearing animals in the Tundra region. It can change its fur from a thick white to a short brown coat depending on the season. The Arctic Fox has been declining in numbers due to over hunting in some areas and the emergence of the large red fox in others. Finland, Norway, and Sweden are some of the areas where the dwindling population of Arctic foxes has been due to over hunting. The emergence of red foxes in their territory is mainly due to the diminishing ice. The red foxes have on occasion competed for the same food and on occasioni have been seen to kill their rival counterparts.",
+            "image":""
+        },
+        {
+            "id":"2",
+            "name":"Polar Bear",
+            "Facts":"",
+            "image":""
+        },
+        {
+            "id":"3",
+            "name":"Arctic Wolf",
+            "Facts":""
+        },
+        {
+            "id":"4",
+            "name":"Beluga Whales",
+            "Facts":"",
+            "image":""
+        },
+        {
+            "id":"5",
+            "name":"Bowhead Whale",
+            "Facts":"",
+            "image":""
+        },
+        {
+            "id":"6",
+            "name":"Prairie Pigeon",
+            "Facts":"",
+            "image":""
+        },
+        {
+            "id":"7",
+            "name":"Arctic Peregrine Falcon",
+            "Facts":"",
+            "image":""
+        },
+        {
+            "id":"8",
+            "name":"Narwhal",
+            "Facts":"",
+            "image":""
+        },
+        {
+            "id":"9",
+            "name":"Caribou",
+            "Facts":"",
+            "image":""
+        },
+        {
+            "id":"10",
+            "name":"Walrus",
+            "Facts":"",
+            "image":""
+        }
+    ]
+}
--- save-arctic/src/pages/EducationPage.js
+++ save-arctic/src/pages/EducationPage.js
@@ -1,7 +1,46 @@
-import NavBar from "../components/layout/NavBar";
+import '../css/EducationPage.css';
+import BootstrapButton from "../components/layout/BootstrapButton";
+import Stack from 'react-bootstrap/Stack';
+import Background from "../img/quizCover.jpg";
+
 
 function EducationPage() {
-    return <div>Education Page</div>;
+    return <div className="eduPage">
+        <Stack gap={3}>
+            <div className="bg-light border">Arctic Animal of the Day</div>
+            <div className="bg-light border">Actions - What can we do as individuals?
+                    <p>
+                    There are many ways in our daily life that we can take practical actions on cutting our carbon footprint. 
+                    Put on an extra layer and turn down the heating a degree or two. Turn off lights and appliances when you 
+                    don't need them to save power. Replace light bulbs with LEDs or other low-energy lights. 
+                    Make simple changes to how you use hot water, like buying a water-efficient shower head. 
+                    Take public transportation or car pooling with others to recude the frequency of using private vehicles. 
+                    </p>
+                </div>
+            <div className="bg-light border">Impacts
+                <p>To keep the temperature increase somewhat close to 2°C above pre-industrial times, the average footprint of everyone 
+                    in the world needs to be approaching to 0 to 2 tonnes by 2050. Your contribution may be a small piece of the pie overall, 
+                    but you alone can prevent many tonnes of GHGs from entering the atmosphere just by changing a few habits. 
+                    By eliminating a quarter or half of your emissions, you could prevent 100’s of tonnes of GHGs from entering the atmosphere 
+                    over the course of your life. Our impact over a lifetime really adds up. If people start to make gradual changes on 
+                    reducing individual carbon emission, we can make a  huge difference on cutting the total amount of emission each year. 
+                    This profound impact can effectively improve the global warming issue and protect the habitat of Arctic wildlife. </p>
+                </div>
+        </Stack>
+        <div className='quizSection' style={{ 
+                width: '100vw',
+                height: '100vh',
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat', 
+                backgroundImage: `url(${Background})`}}>
+            <h1 className="intro">Ready to save more Arctic wildlifes Forever?</h1>
+            <br></br>
+            <BootstrapButton></BootstrapButton>
+        </div>
+       
+       
+    </div>;
 }
 
 export default EducationPage;
