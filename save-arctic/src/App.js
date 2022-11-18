4760244e71fa6349026a6bdc535f95ce5b1e8d2f
Jackie-Si <yujias6@uw.edu>
2022-11-17 16:04:40 -0800

    login page first update

--- save-arctic/src/App.js
+++ save-arctic/src/App.js
@@ -1,4 +1,5 @@
 import { Routes ,Route } from 'react-router-dom';
+import { React, useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/App.css';
 
@@ -7,20 +8,57 @@ import EducationPage from './pages/EducationPage';
 import AccountPage from './pages/AccountPage';
 import Footer from './components/layout/Footer.js';
 import NavBar from './components/layout/NavBar';
-
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar></NavBar>
-        <Routes>
-          <Route path='/' element={<LandingPage />} />
-          <Route exact path='/education' element={<EducationPage />} />
-          <Route exact path='/point' element={<AccountPage />} />
-        </Routes>
-        <Footer></Footer>
-    </div>
-  );
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  //authentication
+  useEffect(() => {
+    const auth = getAuth();
+
+    //addEventListener
+    const unregisterAuthListener = onAuthStateChanged(auth, (firebaseUser) => {
+      if (firebaseUser) {
+        setCurrentUser(firebaseUser);
+      } else {
+        setCurrentUser(null);
+      }
+    });
+
+    return () => {
+      //cleanup
+      unregisterAuthListener();
+    };
+  }, []);
+  //user stays logedin, but need to change the UI
+  console.log(currentUser);
+
+  if (currentUser) {
+    return (
+      <div className="App">
+        <NavBar></NavBar>
+          <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route exact path='/education' element={<EducationPage />} />
+            <Route exact path='/point' element={<AccountPage />} />
+          </Routes>
+          <Footer></Footer>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <NavBar></NavBar>
+          <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route exact path='/education' element={<LandingPage />} />
+            <Route exact path='/point' element={<LandingPage />} />
+          </Routes>
+          <Footer></Footer>
+      </div>
+    )
+  }
 }
 
 export default App;
--- save-arctic/src/pages/Landing.js
+++ save-arctic/src/pages/Landing.js
@@ -1,7 +1,40 @@
+import React from "react";
+import "../css/landing_page.css"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import { getAuth, EmailAuthProvider, GoogleAuthProvider } from "firebase/auth";
 
+const firebaseUIConfig = {
+    signInOptions: [
+      { provider: EmailAuthProvider.PROVIDER_ID, requiredDisplayName: true },
+      GoogleAuthProvider.PROVIDER_ID,
+    ],
+    signInFlow: "popup",
+    credentialHelper: "none",
+    callbacks: {
+      signInSuccessWithAuthResult: () => {
+        return false; 
+      },
+    },
+  };
 
 function LandingPage() {
-    return <div>Landing Page</div>;
+    let auth = getAuth();
+    return (
+        <React.Fragment>
+            {/* banner */}
+            <div className="banner-container">
+                {/* banner image */}
+                <img src={require("../images/banner.jpg")} alt="a variety of arctic animals" />
+                <div id="callout">
+                    <h1>Save the arctic wildlife, take actions today!</h1>
+                    <div>
+                        <p> Get Started </p>
+                        <StyledFirebaseAuth uiConfig={firebaseUIConfig} firebaseAuth={auth} aria-label="sign in" />
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
 }
 
 export default LandingPage;
