c72a8e0f7943a9ee1a520eae849b69ace584f897
张巧依 <zhangqiaoyi@Whitney-Zhang.local>
2022-12-03 16:38:54 -0800

    add edupage

---
+++ save-arctic/src/components/AnimalInfo.js
@@ -0,0 +1,26 @@
+
+import React, { useState, useEffect } from 'react';
+import animalJson from "../data/animals.json";
+
+
+function AnimalInfo() {
+  const animalList = JSON.stringify(animalJson);
+  const data = JSON.parse(animalList);
+
+  const [animal, setAnimal] = useState(data);
+  const [random, setRandom] = useState(Math.floor(Math.random() * 9));
+
+  return (
+  <div className="infoSection">
+      <h4>{animal[random].name}</h4>
+      <br></br>
+      <p><strong> Scientific Name:</strong> {animal[random].scientificName}</p>
+      <br></br>
+      <p><strong> Population:</strong> {animal[random].population}</p>
+      <br></br>
+      <p> {animal[random].fact}</p>
+  </div>
+  );
+};
+
+export default AnimalInfo;
