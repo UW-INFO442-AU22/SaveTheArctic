742619ad3ca7f1d4745bc0c116e77601a0078dd3
Catherine Miao <md0615@uw.edu>
2022-12-04 18:14:52 -0800

    add quiz in navbar

--- save-arctic/src/components/progressbar.js
+++ save-arctic/src/components/progressbar.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import ProgressBar from 'react-bootstrap/ProgressBar';
+import { getAuth } from "firebase/auth";
 
 function Bar() {
+  const [myPoint, setMyPoint] = useState([]);
+  let user = getAuth().currentUser;
+
   const now = 60;
+
   return (
     <div class='container'>
       <ProgressBar now={now} label={`${now}%`} />
       <p id="pointNotification">points to your next free donation!</p>
+      <p>{user.email}</p>
     </div>
   );
 }
